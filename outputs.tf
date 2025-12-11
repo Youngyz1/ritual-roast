741a4a3ed5d068e7e6727e4bf3bb1a93eb9f184d
Youngyz1 <uche2net@gmail.com>
2025-12-11 18:58:05 +0000

    now

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "alb_dns_name" {
 
 output "ecr_repository_url" {
   description = "ECR repository URL for Ritual Roast image"
-  value       = aws_ecr_repository.app.repository_url
+  value       = aws_ecr_repository.ritual.repository_url
 }
 
 output "ecs_cluster_name" {
@@ -15,13 +15,14 @@ output "ecs_cluster_name" {
 
 output "ecs_service_name" {
   description = "Name of the ECS service"
-  value       = aws_ecs_service.app.name
+  value       = aws_ecs_service.ritual_service.name
 }
 
 output "db_endpoint" {
   description = "RDS MySQL endpoint"
   value       = aws_db_instance.mysql.address
 }
+
 output "cloudfront_domain" {
   description = "CloudFront distribution domain"
   value       = aws_cloudfront_distribution.ritual_roast_cdn.domain_name

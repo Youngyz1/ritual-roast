59c29397ad5f50342e8b487f1b3068ba293b5fd2
Youngyz1 <uche2net@gmail.com>
2025-12-11 18:39:51 +0000

    updated ecs.tf

--- ecr_ecs.tf
+++ ecr_ecs.tf
@@ -14,7 +14,44 @@ resource "aws_ecr_repository" "ritual" {
 }
 
 ########################################
-# IAM POLICY – ECS task needs SecretsManager access
+# IAM ROLES (Execution + Task)
+########################################
+
+# ECS Execution Role – allows ECS to pull ECR images & write logs
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ritual-roast-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_default" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS Task Role – app permissions (your container uses this)
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ritual-roast-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+########################################
+# IAM POLICY – allow ECS Task to read the secret
 ########################################
 resource "aws_iam_policy" "ecs_secrets_policy" {
   name = "ritual-roast-ecs-secrets-policy"
@@ -28,7 +65,6 @@ resource "aws_iam_policy" "ecs_secrets_policy" {
           "secretsmanager:GetSecretValue",
           "kms:Decrypt"
         ],
-        # Allow ECS to read ONLY THIS secret version
         Resource = aws_secretsmanager_secret_version.db_version.arn
       }
     ]
@@ -36,7 +72,7 @@ resource "aws_iam_policy" "ecs_secrets_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_secrets_attach" {
-  role       = aws_iam_role.ecs_execution_role.name
+  role       = aws_iam_role.ecs_task_role.name
   policy_arn = aws_iam_policy.ecs_secrets_policy.arn
 }
 
@@ -81,7 +117,6 @@ resource "aws_ecs_task_definition" "ritual_task" {
         { name = "AWS_REGION", value = var.region }
       ]
 
-      # Secrets from Secrets Manager
       secrets = [
         {
           name      = "DB_USERNAME"

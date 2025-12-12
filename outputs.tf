8068c30247fb8e35220087388b5650eda0291891
Youngyz1 <uche2net@gmail.com>
2025-12-12 21:29:17 +0000

    edited deploy.yml

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "alb_dns_name" {
   description = "Public DNS of the ALB"
-  value       = aws_lb.app.dns_name
+  value       = try(aws_lb.app.dns_name, null)
 }
 
 output "ecr_repository_url" {
   description = "ECR repository URL for Ritual Roast image"
-  value       = aws_ecr_repository.ritual.repository_url
+  value       = try(aws_ecr_repository.ritual.repository_url, null)
 }
 
 output "ecs_cluster_name" {
   description = "Name of the ECS cluster"
-  value       = aws_ecs_cluster.this.name
+  value       = try(aws_ecs_cluster.this.name, null)
 }
 
 output "ecs_service_name" {
   description = "Name of the ECS service"
-  value       = aws_ecs_service.ritual_service.name
+  value       = try(aws_ecs_service.ritual_service.name, null)
 }
 
 output "db_endpoint" {
   description = "RDS MySQL endpoint"
-  value       = aws_db_instance.mysql.address
+  value       = try(aws_db_instance.mysql.address, null)
 }
 
 output "cloudfront_domain" {
   description = "CloudFront distribution domain"
-  value       = aws_cloudfront_distribution.ritual_roast_cdn.domain_name
+  value       = try(aws_cloudfront_distribution.ritual_roast_cdn.domain_name, null)
 }

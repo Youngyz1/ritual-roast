d5e2301b9c79344a35a8f10b944cbba5a6d0d2de
Youngyz <uche2net@gmail.com>
2025-12-22 20:07:20 +0100

    updated

--- s3bucket.tf
+++ s3bucket.tf
@@ -1,13 +1,14 @@
 resource "aws_s3_bucket" "rr_alb_logs" {
-  bucket        = "ritual-roast-alb-logs"  # No random suffix
+  bucket        = "ritual-roast-alb-logs-${random_id.bucket_suffix.hex}"
   force_destroy = true
 
   tags = {
     Name = "ritual-roast-alb-logs"
   }
-}
 
-resource "aws_s3_bucket_acl" "rr_alb_logs_acl" {
-  bucket = aws_s3_bucket.rr_alb_logs.id
-  acl    = "private"
+  # Optional: enable block public access for safety
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
